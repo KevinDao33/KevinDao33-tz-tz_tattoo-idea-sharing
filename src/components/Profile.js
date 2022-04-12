ace750d9285b677a2e061e5e6330ace10d45122c
KevinDao <kevinph.chiou@gmail.com>
2022-04-13 00:56:14 +0800

    add user info/ render pins at my pin/ switch between my pin and my collection

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,104 @@
 import React, { useState, useEffect } from "react";
-import {PorfileWrapper} from "../styles/Profile.module"
+import {
+  PorfileWrapper,
+  UserImage,
+  UserName,
+  ShowFollow,
+  ButtonWeapper,
+  Button,
+  UserStuffWrapper,
+  SelectSection,
+  UserPin,
+} from "../styles/Profile.module";
+import {
+  AllPinsWrapper,
+  PinWrapper,
+  PinImage,
+} from "../styles/Homepage.module";
+
+import chicken from "../test-images/chicken.jpg";
+import kitty from "../test-images/kitty.jpg";
+import wolf from "../test-images/wolf.jpg";
+import flower from "../test-images/flower.jpg";
+import bear from "../test-images/bear.jpg";
+import cherry from "../test-images/cherry.jpg";
+import death from "../test-images/death.jpg";
+import frog from "../test-images/frog.jpg";
+import guitarfrog from "../test-images/guitar-frog.jpg";
+import hands from "../test-images/hands.jpg";
+import raccoon from "../test-images/raccoon.jpg";
+import trex from "../test-images/t-rex.jpg";
+import uglyflower from "../test-images/ugly-flower.jpg";
+import skateboard from "../test-images/skateboard.jpg";
+import duck from "../test-images/duck.jpg";
+
+const allPins = [
+  { pinImage: chicken },
+  { pinImage: kitty },
+  { pinImage: wolf },
+  { pinImage: flower },
+  { pinImage: bear },
+  { pinImage: cherry },
+  { pinImage: death },
+  { pinImage: frog },
+  { pinImage: guitarfrog },
+  { pinImage: hands },
+  { pinImage: raccoon },
+  { pinImage: trex },
+  { pinImage: uglyflower },
+  { pinImage: skateboard },
+  { pinImage: duck },
+];
 
 function Profile() {
+  const [showSection, setShowSection] = useState(0);
+  // 0=> my pin/ 1=> my collection/ 2=> my schedual(artist only)
+  const [pins, setPins] = useState(allPins);
+
+  const showMyPin = () => {
+    setShowSection(0);
+  };
+  const showMyCollection = () => {
+    setShowSection(1);
+  };
+  const showMySchedule = () => {
+    setShowSection(2);
+  };
+
   return (
     <>
       <PorfileWrapper>
-          <h1>welcome to profile page</h1>
+        <UserImage></UserImage>
+        <UserName>Kevin Dao</UserName>
+        <ShowFollow>{2} following</ShowFollow>
+        <ShowFollow>{5} follower</ShowFollow>
+        <ButtonWeapper>
+          <Button>share</Button>
+          <Button>edit</Button>
+        </ButtonWeapper>
+
+        <UserStuffWrapper>
+          <ButtonWeapper>
+            <SelectSection onClick={showMyPin}>my pin</SelectSection>
+            <SelectSection onClick={showMyCollection}>
+              my collection
+            </SelectSection>
+          </ButtonWeapper>
+
+          {showSection === 0 ? (
+            <AllPinsWrapper>
+              {pins &&
+                pins.map((pin, index) => (
+                  <PinWrapper key={index}>
+                    <PinImage src={pin.pinImage} />
+                  </PinWrapper>
+                ))}
+            </AllPinsWrapper>
+          ) : (
+            <div>welcome to collection</div>
+          )}
+          
+        </UserStuffWrapper>
       </PorfileWrapper>
     </>
   );
--- src/styles/Profile.module.js
+++ src/styles/Profile.module.js
@@ -5,7 +5,79 @@ const PorfileWrapper = styled.div`
   margin: 15px auto;
   border: 1px solid red;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
 `;
 
-export {PorfileWrapper};
+const UserImage = styled.div`
+  border-radius: 50px;
+  width: 100px;
+  height: 100px;
+  background-color: gray;
+  margin: 20px auto 0px auto;
+`;
+
+const UserName = styled.p`
+  font-size: 1.5rem;
+  margin: 15px auto 10px auto;
+  font-weight: bold;
+`;
+
+const ShowFollow = styled.p`
+  font-size: 1rem;
+  margin: 2px auto;
+`;
+
+const ButtonWeapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 10px auto;
+`;
+
+const Button = styled.button`
+  color: gray;
+  border: none;
+  border-radius: 30px;
+  padding: 10px;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  font-size: 1rem;
+  margin: 10px;
+`;
+
+const UserStuffWrapper = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid purple;
+`;
+
+const SelectSection = styled.p`
+  font-size: 1rem;
+  margin: 10px 30px;
+  border-bottom: 1px solid gray;
+`;
+
+const UserPin = styled.div`
+  border: 1px solid green;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: flex-start;
+  height: auto;
+  min-height: 300px;
+`;
+
+export {
+  PorfileWrapper,
+  UserImage,
+  UserName,
+  ShowFollow,
+  ButtonWeapper,
+  Button,
+  UserStuffWrapper,
+  SelectSection,
+  UserPin,
+};
